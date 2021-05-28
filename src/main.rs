e46bd00b326f51bacfc501c624b6486136675aab
eeff <eeff@local>
2021-05-29 00:31:45 +0800

    Add telemetry using the tracing ecosystem

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,15 @@
 use sqlx::PgPool;
 use zero2prod::configuration::get_configuration;
 use zero2prod::startup::run;
+use zero2prod::telemetry::{get_subscriber, init_subscriber};
 
 use std::net::TcpListener;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+    let subcriber = get_subscriber("zero2prod".into(), "info".into(), std::io::stdout);
+    init_subscriber(subcriber);
+
     let configuration = get_configuration().expect("Failed to read configuration.");
     let address = format!("127.0.0.1:{}", configuration.application_port);
     let connection_string = configuration.database.connection_string();
--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,8 +1,24 @@
+use once_cell::sync::Lazy;
 use sqlx::{Connection, Executor, PgConnection, PgPool};
 use std::net::TcpListener;
 use uuid::Uuid;
 
 use zero2prod::configuration::{get_configuration, DatabaseSettings};
+use zero2prod::startup::run;
+use zero2prod::telemetry::{get_subscriber, init_subscriber};
+
+// Ensure the `tracing` stack is only initialised once
+static TRACING: Lazy<()> = Lazy::new(|| {
+    let default_filter_level = "debug".into();
+    let subscriber_name = "test".into();
+    if std::env::var("TEST_LOG").is_ok() {
+        let subscriber = get_subscriber(subscriber_name, default_filter_level, std::io::stdout);
+        init_subscriber(subscriber);
+    } else {
+        let subscriber = get_subscriber(subscriber_name, default_filter_level, std::io::sink);
+        init_subscriber(subscriber);
+    }
+});
 
 #[actix_rt::test]
 async fn health_check_works() {
@@ -31,6 +47,8 @@ pub struct TestApp {
 
 /// Launch our application in the background.
 async fn spawn_app() -> TestApp {
+    Lazy::force(&TRACING);
+
     // Bind port 0 trigger an OS scan for an available port
     let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind random port");
     // Retrieve the port assigned to us by the OS
@@ -40,8 +58,7 @@ async fn spawn_app() -> TestApp {
     let mut configuration = get_configuration().expect("Failed to read configuration.");
     configuration.database.database_name = Uuid::new_v4().to_string();
     let connection_pool = configure_database(&configuration.database).await;
-    let server = zero2prod::startup::run(listener, connection_pool.clone())
-        .expect("Failed to listen to address");
+    let server = run(listener, connection_pool.clone()).expect("Failed to listen to address");
 
     // Launch the server as a background task
     let _ = tokio::spawn(server);
