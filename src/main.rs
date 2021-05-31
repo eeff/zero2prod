db9ffea5c9acad41b1e0635a447b03d8932add5d
eeff <eeff@local>
2021-05-31 20:33:16 +0800

    Add email client to actix-web app data

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use sqlx::postgres::PgPool;
 use zero2prod::configuration::get_configuration;
+use zero2prod::email_client::EmailClient;
 use zero2prod::startup::run;
 use zero2prod::telemetry::{get_subscriber, init_subscriber};
 
@@ -20,5 +21,11 @@ async fn main() -> std::io::Result<()> {
         .expect("Failed to connect to Postgres.");
     let listener = TcpListener::bind(address).expect("Failed to bind address");
 
-    run(listener, connection_pool)?.await
+    let sender_email = configuration
+        .email_client
+        .sender()
+        .expect("Invalid sender email address.");
+    let email_client = EmailClient::new(configuration.email_client.base_url, sender_email);
+
+    run(listener, connection_pool, email_client)?.await
 }
