34b4efc203633bddc45a07ea439b4ad3ef33e997
eeff <eeff@local>
2021-06-01 19:54:55 +0800

    Refactor startup logic

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,6 @@
-use sqlx::postgres::PgPool;
 use zero2prod::configuration::get_configuration;
-use zero2prod::email_client::EmailClient;
-use zero2prod::startup::run;
+use zero2prod::startup::Application;
 use zero2prod::telemetry::{get_subscriber, init_subscriber};
-
-use std::net::TcpListener;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
@@ -12,24 +8,7 @@ async fn main() -> std::io::Result<()> {
     init_subscriber(subcriber);
 
     let configuration = get_configuration().expect("Failed to read configuration.");
-    let address = format!(
-        "{}:{}",
-        configuration.application.host, configuration.application.port
-    );
-    let connection_pool = PgPool::connect_with(configuration.database.with_db())
-        .await
-        .expect("Failed to connect to Postgres.");
-    let listener = TcpListener::bind(address).expect("Failed to bind address");
-
-    let sender_email = configuration
-        .email_client
-        .sender()
-        .expect("Invalid sender email address.");
-    let email_client = EmailClient::new(
-        configuration.email_client.base_url,
-        sender_email,
-        configuration.email_client.authorization_token,
-    );
-
-    run(listener, connection_pool, email_client)?.await
+    let application = Application::build(configuration).await?;
+    application.run_until_stopped().await?;
+    Ok(())
 }
--- tests/api/helpers.rs
+++ tests/api/helpers.rs
@@ -1,11 +1,9 @@
 use once_cell::sync::Lazy;
 use sqlx::{Connection, Executor, PgConnection, PgPool};
-use std::net::TcpListener;
 use uuid::Uuid;
 
 use zero2prod::configuration::{get_configuration, DatabaseSettings};
-use zero2prod::email_client::EmailClient;
-use zero2prod::startup::run;
+use zero2prod::startup::{get_connection_pool, Application};
 use zero2prod::telemetry::{get_subscriber, init_subscriber};
 
 // Ensure the `tracing` stack is only initialised once
@@ -42,33 +40,31 @@ impl TestApp {
 pub async fn spawn_app() -> TestApp {
     Lazy::force(&TRACING);
 
-    // Bind port 0 trigger an OS scan for an available port
-    let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind random port");
-    // Retrieve the port assigned to us by the OS
-    let port = listener.local_addr().unwrap().port();
-    let address = format!("http://127.0.0.1:{}", port);
+    // Randomise configuration to ensure test isolation
+    let configuration = {
+        let mut c = get_configuration().expect("Failed to read configuration.");
+        c.database.database_name = Uuid::new_v4().to_string();
+        // Bind port 0 trigger an OS scan for an available port
+        c.application.port = 0;
+        c
+    };
 
-    let mut configuration = get_configuration().expect("Failed to read configuration.");
-    configuration.database.database_name = Uuid::new_v4().to_string();
-    let connection_pool = configure_database(&configuration.database).await;
-    let sender_email = configuration
-        .email_client
-        .sender()
-        .expect("Invalid sender email address.");
-    let email_client = EmailClient::new(
-        configuration.email_client.base_url,
-        sender_email,
-        configuration.email_client.authorization_token,
-    );
-    let server =
-        run(listener, connection_pool.clone(), email_client).expect("Failed to listen to address");
+    configure_database(&configuration.database).await;
 
+    let application = Application::build(configuration.clone())
+        .await
+        .expect("Failed to build application");
+
+    // Get the port before spawning the application
+    let address = format!("http://127.0.0.1:{}", application.port());
     // Launch the server as a background task
-    let _ = tokio::spawn(server);
+    let _ = tokio::spawn(application.run_until_stopped());
 
     TestApp {
         address,
-        db_pool: connection_pool,
+        db_pool: get_connection_pool(&configuration.database)
+            .await
+            .expect("Failed to connect to database."),
     }
 }
 
